182d64a129606b8e2a199dec746603342680d8b2
Adii <77727680+AdityaMotale@users.noreply.github.com>
2025-09-13 12:02:50 +0530

    updated dependencies (nix flake)

--- flake.nix
+++ flake.nix
@@ -1,33 +1,77 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default =
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells = {
+        rs = pkgs.mkShell {
+          name = "dev-rust";
+          buildInputs = with pkgs; [
+            gcc
+            pkg-config
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+
+          shellHook = ''
+            export RUST_BACKTRACE=1
+              
+            echo " : $(rustc --version | cut -d' ' -f2)"
+          '';
         };
-      in
-      pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nasm
-          gcc
-          gcc.libc
-          gdb
-          glibc.static
-          perf
-          rustc
-          cargo
-          rustfmt
-          rust-analyzer
-          clippy
-          cargo-show-asm
-          nodejs
-          typescript
-          python3
-        ];
+        py = pkgs.mkShell {
+          name = "dev-python";
+          buildInputs = with pkgs; [
+            gcc
+            pkg-config
+            python314
+            ruff
+            uv
+            pyright
+          ];
+
+           shellHook = ''
+            echo " : $(python3 --version)"
+          '';
+        };
+        js = pkgs.mkShell {
+          name = "dev-js";
+          buildInputs = with pkgs; [
+            gcc
+            pkg-config
+            nodejs
+            typescript
+          ];
+
+           shellHook = ''
+            echo " : $(node --version)"
+          '';
+        };
+        c = pkgs.mkShell {
+          name = "dev-c";
+          buildInputs = with pkgs; [
+            nasm
+            gcc
+            gcc.libc
+            gdb
+            glibc.static
+            perf
+          ];
+
+          shellHook = ''
+            echo " : $(gcc --version)"
+          '';
+        };
       };
-  };
+     }
+  );
 }
