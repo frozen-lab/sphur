90625cc68929bedc1e0774c3967a223b0448bc48
Adii <77727680+AdityaMotale@users.noreply.github.com>
2025-09-16 20:36:31 +0530

    (asm) SIMD utils for x86 w/ seed generation on `macos_x64`

--- asm/macos_x64.asm
+++ asm/macos_x64.asm
@@ -1,11 +1,24 @@
 bits 64
 default rel
 
-global _asm_add_one
+global _function_gen_seeds
+
+; simd uitls (`./x64_simd.asm`)
+extern function_split_mix_64
 
 section .text
-_asm_add_one:
-        mov rax, rdi
-        add rax, 0x01
+
+; Generate N independent 64-bit "sub-seeds" based on a input seed
+;
+; Args:
+;   rdi - Initial 64-bit seed
+;   rsi - Pointer to output buffer (must be 8 * N bytes long)
+;   rdx - No. of sub-seeds to generate (u64 & >0)
+;
+; Retruns:
+;   rax - `1` on error, `0` otherwise
+;   rsi - (preserved) the input pointer is preserved, w/ the buffer
+;         containing N 64-bit sub-seeds
+_function_gen_seeds:
+        call function_split_mix_64
         ret
-        
--- clib/src/sphur.c
+++ clib/src/sphur.c
@@ -2,13 +2,13 @@
 
 // ASM Function: Generates N 64-bit sub-seeds based on input seed
 // Returns: 0 = ok, 1 = error
-int function_split_mix_64(uint64_t seed, uint64_t *out, uint64_t n);
+int function_gen_seeds(uint64_t seed, uint64_t *out, uint64_t n);
 
 int sphur_init(sphur_state_t *state, uint64_t seed) {
   // error (invalid state pointer)
   if (!state)
     return 1;
 
-  int ret = function_split_mix_64(seed, state->seeds, 8);
+  int ret = function_gen_seeds(seed, state->seeds, 8);
   return ret;
 }
