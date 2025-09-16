c6efb4509a9d148396db6be883970fa5cd793337
Adii <77727680+AdityaMotale@users.noreply.github.com>
2025-09-17 01:07:09 +0530

    (asm) added default seed generation for `win_x64`

--- asm/win_x64.asm
+++ asm/win_x64.asm
@@ -5,6 +5,7 @@ global function_gen_seeds
 
 ; simd uitls (`./x64_simd.asm`)
 extern function_split_mix_64
+extern QueryPerformanceCounter
 
 section .text
 
@@ -16,17 +17,31 @@ section .text
 ;   r8 - No. of sub-seeds to generate (u64 & >0)
 ;
 ; Retruns:
-;   rax - `1` on error, `0` otherwise
+;   rax - `0` on success, `1` otherwise
 ;   rdx - (preserved) the input pointer is preserved, w/ the buffer
 ;         containing N 64-bit sub-seeds
 function_gen_seeds:
+        test rcx, rcx
+        jnz .split_mix
+
+        sub rsp, 0x28                   ; shadow space
+        lea rcx, [rsp + 0x20]           ; pointer to 64-bit buffer
+
+        ;; this never fails ╰(*°▽°*)╯
+        ;; let's believe in microsoft, for more info ->
+        ;; `https://learn.microsoft.com/en-us/windows/win32/api/profileapi/nf-profileapi-queryperformancecounter`
+        call QueryPerformanceCounter
+        
+        mov rcx, [rsp+0x20]             ; seed = performance counter
+        add rsp, 0x28                   ; clear shadow space
+.split_mix:
         ;; prepare args for split_mix (System V style)
         mov rdi, rcx
         mov rsi, rdx
         mov rdx, r8
 
-        sub rsp, 0x20                ; creating shadow space
+        sub rsp, 0x20                   ; creating shadow space
         call function_split_mix_64
-        add rsp, 0x20                ; destroying shadow space
+        add rsp, 0x20                   ; clear shadow space
         
         ret
