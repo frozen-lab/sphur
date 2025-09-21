4787d606f1b639d643d9cd54229432655966bb83
Adii <77727680+AdityaMotale@users.noreply.github.com>
2025-09-21 23:14:55 +0530

    impl of ISA/cpuid detection

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,56 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![allow(unused)]
+
+/// Sphūr is SIMD accelerated Pseudo-Random Number Generator.
+pub struct Sphur {
+    /// Isa (Instruction Set Architecture) available at runtime
+    isa: ISA,
+
+    /// Current idx of the state being used for twistter
+    idx: usize,
+}
+
+impl Sphur {
+    pub fn new() -> SphurResult<Self> {
+        Ok(Self {
+            isa: detect_isa(),
+            idx: 0,
+        })
+    }
+}
+
+/// Custom result type for [Sphur]
+pub type SphurResult<T> = Result<T, SphurError>;
+
+/// Types of Error exposed by [Sphur]
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub enum SphurError {
+    InitError,
+}
+
+enum ISA {
+    // This SIMD ISA is an upgrade over SSE2 if available at runtime
+    AVX2,
+
+    // This SIMD ISA is default on x64 (x86_64), as it's virtually
+    // available on all x64 CPU's
+    SSE2,
+
+    // Neon is vurtually available on all aarch64 CPU's
+    NEON,
+}
+
+#[cfg(target_arch = "x86_64")]
+fn detect_isa() -> ISA {
+    if is_x86_feature_detected!("avx2") {
+        return ISA::AVX2;
+    }
+
+    ISA::SSE2
+}
+
+#[cfg(target_arch = "aarch64")]
+fn detect_isa() -> ISA {
+    ISA::NEON
 }
 
 #[cfg(test)]
@@ -7,8 +58,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_detect_isa_returns_valid_value() {
+        let isa = detect_isa();
+        match isa {
+            ISA::AVX2 | ISA::SSE2 | ISA::NEON => {}
+            _ => panic!("Unknown ISA detected"),
+        }
     }
 }
